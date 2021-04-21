52f82d08969ed1d0b4ecc0b504ea99fbfdf7cf49
jowenshaw <jowenshaw@gmail.com>
2021-04-21 12:22:51 +0800

    update check config

--- params/config.go
+++ params/config.go
@@ -9,7 +9,10 @@ import (
 	"github.com/anyswap/CrossChain-Bridge/log"
 )
 
-var scanConfig = &ScanConfig{}
+var (
+	configFile string
+	scanConfig = &ScanConfig{}
+)
 
 // ScanConfig scan config
 type ScanConfig struct {
@@ -30,27 +33,22 @@ func (c *TokenConfig) IsNativeToken() bool {
 	return c.TokenAddress == "native"
 }
 
-// CheckConfig check token config
-func (c *TokenConfig) CheckConfig() error {
-	return nil // TODO
-}
-
 // GetScanConfig get scan config
 func GetScanConfig() *ScanConfig {
 	return scanConfig
 }
 
 // LoadConfig load config
-func LoadConfig(configFile string) *ScanConfig {
-	log.Println("Config file is", configFile)
-	if !common.FileExist(configFile) {
-		log.Fatalf("LoadConfig error: config file '%v' not exist", configFile)
+func LoadConfig(filePath string) *ScanConfig {
+	log.Println("LoadConfig Config file is", filePath)
+	if !common.FileExist(filePath) {
+		log.Fatalf("LoadConfig error: config file '%v' not exist", filePath)
 	}
+
 	config := &ScanConfig{}
-	if _, err := toml.DecodeFile(configFile, &config); err != nil {
+	if _, err := toml.DecodeFile(filePath, &config); err != nil {
 		log.Fatalf("LoadConfig error (toml DecodeFile): %v", err)
 	}
-	scanConfig = config // init scan config
 
 	var bs []byte
 	if log.JSONFormat {
@@ -60,21 +58,68 @@ func LoadConfig(configFile string) *ScanConfig {
 	}
 	log.Println("LoadConfig finished.", string(bs))
 
-	if err := CheckConfig(); err != nil {
-		log.Fatalf("Check config failed. %v", err)
+	if err := config.CheckConfig(); err != nil {
+		log.Fatalf("LoadConfig Check config failed. %v", err)
 	}
+
+	configFile = filePath // init config file path
+	scanConfig = config   // init scan config
 	return scanConfig
 }
 
-// CheckConfig check config
-func CheckConfig() (err error) {
-	if len(scanConfig.Tokens) == 0 {
+// ReloadConfig reload config
+func ReloadConfig() {
+	log.Println("ReloadConfig Config file is", configFile)
+	if !common.FileExist(configFile) {
+		log.Errorf("ReloadConfig error: config file '%v' not exist", configFile)
+		return
+	}
+
+	config := &ScanConfig{}
+	if _, err := toml.DecodeFile(configFile, &config); err != nil {
+		log.Errorf("ReloadConfig error (toml DecodeFile): %v", err)
+		return
+	}
+
+	if err := config.CheckConfig(); err != nil {
+		log.Errorf("ReloadConfig Check config failed. %v", err)
+		return
+	}
+	log.Println("ReloadConfig success.")
+	scanConfig = config // reassign scan config
+}
+
+// CheckConfig check scan config
+func (c *ScanConfig) CheckConfig() (err error) {
+	if len(c.Tokens) == 0 {
 		return errors.New("no token config exist")
 	}
-	for _, tokenCfg := range scanConfig.Tokens {
+	for _, tokenCfg := range c.Tokens {
 		err = tokenCfg.CheckConfig()
 		if err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+// CheckConfig check token config
+func (c *TokenConfig) CheckConfig() error {
+	if c.PairID == "" {
+		return errors.New("empty 'PairID'")
+	}
+	if c.SwapServer == "" {
+		return errors.New("empty 'SwapServer'")
+	}
+	if !c.IsNativeToken() && !common.IsHexAddress(c.TokenAddress) {
+		return errors.New("wrong 'TokenAddress' " + c.TokenAddress)
+	}
+	if c.DepositAddress != "" && !common.IsHexAddress(c.DepositAddress) {
+		return errors.New("wrong 'DepositAddress' " + c.DepositAddress)
+	}
+	for _, topic := range c.LogTopics {
+		if common.HexToHash(topic).Hex() != topic {
+			return errors.New("wrong 'LogTopics' " + topic)
 		}
 	}
 	return nil
